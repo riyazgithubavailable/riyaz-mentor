d773238067b687277d6c66735e9d55e29580b18b
riyazgithubavailable <riyazisheikh97@gmail.com>
2025-04-16 10:43:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Extension from './components/Extension'
 import Navbaar from './components/navbaar'
+
 import useColor from './customHook/useColor'
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
 
   return (
     < div className={` h-full lg:h-screen  overflow-y-hidden ${bgColor.primary}`}>
-   <Navbaar />
-   <Extension />
+        <Navbaar />
+        <Extension />
     </div>
   )
 }
